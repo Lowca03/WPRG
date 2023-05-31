9819241723cecbe792950b504899e28dbf6deff4
Lowca03 <Lowcasmokow12@gmail.com>
2023-05-31 11:36:40 +0200

    Główna zrobiona

--- Lab08/zad 1.php
+++ Lab08/zad 1.php
@@ -1,1 +1,18 @@
-<?php
+<?php //session_start();?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Zadanie z bazami</title>
+</head>
+<body>
+<table>
+    <tr>
+        <th>|<a href="glowna.php"> Strona Główna</a> |</th>
+        <th> <a href="wszystkie.php">Wszystkie samochody</a> |</th>
+        <th> <a href="wszystkie.php">Dodaj samochód</a> |</th>
+    </tr>
+</table>
+</body>
+</html>
+<?php //session_destroy()?>
